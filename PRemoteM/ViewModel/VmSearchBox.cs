a433a600a705302bda9d6de615208d4af7d75568
Shawn <singlex@foxmail.com>
2020-04-15 20:01:22 +0800

    some detail fixed
    Signed-off-by: Shawn <singlex@foxmail.com>

--- PRemoteM/ViewModel/VmSearchBox.cs
+++ PRemoteM/ViewModel/VmSearchBox.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Controls;
@@ -129,69 +131,105 @@ namespace PRM.ViewModel
         private void UpdateDispList(string keyWord)
         {
             DispServerList.Clear();
-            // match keyword
-            foreach (var item in Global.GetInstance().ServerList.Where(x => x.GetType() != typeof(ProtocolServerNone)))
+
+            if (!string.IsNullOrEmpty(keyWord))
             {
-                Debug.Assert(!string.IsNullOrEmpty(item.ClassVersion));
-                Debug.Assert(!string.IsNullOrEmpty(item.ServerType));
-                var f1 = KeyWordMatchHelper.IsMatchPinyinKeyWords(item.DispName, keyWord, out var m1);
-                var f2 = KeyWordMatchHelper.IsMatchPinyinKeyWords(item.GetSubTitle(), keyWord, out var m2);
-                if (f1 || f2)
+                var keyWords = keyWord.Split(new string[]{" "}, StringSplitOptions.RemoveEmptyEntries);
+                var keyWordIsMatch = new List<bool>(keyWords.Length);
+                for (var i = 0; i < keyWords.Length; i++)
+                    keyWordIsMatch.Add(false);
+
+                // match keyword
+                foreach (var item in Global.GetInstance().ServerList.Where(x => x.GetType() != typeof(ProtocolServerNone)))
                 {
-                    var semite = new ProtocolServerBaseInSearchBox(item);
-                    const bool enableHighLine = true;
-                    // highline matched chars.
-                    if (enableHighLine)
+                    Debug.Assert(!string.IsNullOrEmpty(item.ClassVersion));
+                    Debug.Assert(!string.IsNullOrEmpty(item.ServerType));
+
+                    var mDispName = new List<List<bool>>();
+                    var mSubTitle = new List<List<bool>>();
+                    var dispName = item.DispName;
+                    var subTitle = item.GetSubTitle();
+                    for (var i = 0; i < keyWordIsMatch.Count; i++)
                     {
-                        if (f1)
-                        {
-                            var sp = new StackPanel() { Orientation = System.Windows.Controls.Orientation.Horizontal };
-                            var dispName = item.DispName;
-                            for (int i = 0; i < m1.Count; i++)
-                            {
-                                if (m1[i])
-                                    sp.Children.Add(new TextBlock()
-                                    {
-                                        Text = dispName[i].ToString(),
-                                        Background = new SolidColorBrush(Color.FromArgb(80, 239, 242, 132)),
-                                    });
-                                else
-                                    sp.Children.Add(new TextBlock()
-                                    {
-                                        Text = dispName[i].ToString(),
-                                    });
-                            }
-
-                            semite.DispNameControl = sp;
-                        }
-
-                        if (f2)
-                        {
-                            var sp = new StackPanel() { Orientation = System.Windows.Controls.Orientation.Horizontal };
-                            var subtitle = item.SubTitle;
-                            for (int i = 0; i < m2.Count; i++)
-                            {
-                                if (m2[i])
-                                    sp.Children.Add(new TextBlock()
-                                    {
-                                        Text = subtitle[i].ToString(),
-                                        Background = new SolidColorBrush(Color.FromArgb(120, 239, 242, 132)),
-                                    });
-                                else
-                                    sp.Children.Add(new TextBlock()
-                                    {
-                                        Text = subtitle[i].ToString(),
-                                    });
-                            }
-
-                            semite.SubTitleControl = sp;
-                        }
+                        var f1 = KeyWordMatchHelper.IsMatchPinyinKeyWords(dispName, keyWords[i], out var m1);
+                        var f2 = KeyWordMatchHelper.IsMatchPinyinKeyWords(subTitle, keyWords[i], out var m2);
+                        mDispName.Add(m1);
+                        mSubTitle.Add(m2);
+                        keyWordIsMatch[i] = f1 || f2;
                     }
 
-                    DispServerList.Add(semite);
+                    if (keyWordIsMatch.All(x => x == true))
+                    {
+                        var m1 = new List<bool>();
+                        var m2 = new List<bool>();
+                        for (var i = 0; i < dispName.Length; i++)
+                            m1.Add(false);
+                        for (var i = 0; i < subTitle.Length; i++)
+                            m2.Add(false);
+                        for (var i = 0; i < keyWordIsMatch.Count; i++)
+                        {
+                            if (mDispName[i] != null)
+                                for (int j = 0; j < mDispName[i].Count; j++)
+                                    m1[j] |= mDispName[i][j];
+                            if (mSubTitle[i] != null)
+                                for (int j = 0; j < mSubTitle[i].Count; j++)
+                                    m2[j] |= mSubTitle[i][j];
+                        }
+
+
+                        var semite = new ProtocolServerBaseInSearchBox(item);
+                        const bool enableHighLine = true;
+                        // highline matched chars.
+                        if (enableHighLine)
+                        {
+                            if (m1.Any(x => x == true))
+                            {
+                                var sp = new StackPanel() { Orientation = System.Windows.Controls.Orientation.Horizontal };
+                                for (int i = 0; i < m1.Count; i++)
+                                {
+                                    if (m1[i])
+                                        sp.Children.Add(new TextBlock()
+                                        {
+                                            Text = dispName[i].ToString(),
+                                            Background = new SolidColorBrush(Color.FromArgb(80, 239, 242, 132)),
+                                        });
+                                    else
+                                        sp.Children.Add(new TextBlock()
+                                        {
+                                            Text = dispName[i].ToString(),
+                                        });
+                                }
+
+                                semite.DispNameControl = sp;
+                            }
+
+                            if (m2.Any(x => x == true))
+                            {
+                                var sp = new StackPanel() { Orientation = System.Windows.Controls.Orientation.Horizontal };
+                                var subtitle = item.SubTitle;
+                                for (int i = 0; i < m2.Count; i++)
+                                {
+                                    if (m2[i])
+                                        sp.Children.Add(new TextBlock()
+                                        {
+                                            Text = subtitle[i].ToString(),
+                                            Background = new SolidColorBrush(Color.FromArgb(120, 239, 242, 132)),
+                                        });
+                                    else
+                                        sp.Children.Add(new TextBlock()
+                                        {
+                                            Text = subtitle[i].ToString(),
+                                        });
+                                }
+
+                                semite.SubTitleControl = sp;
+                            }
+                        }
+
+                        DispServerList.Add(semite);
+                    }
                 }
             }
-
 
             if (!DispServerList.Any())
                 PopupIsOpen = false;
