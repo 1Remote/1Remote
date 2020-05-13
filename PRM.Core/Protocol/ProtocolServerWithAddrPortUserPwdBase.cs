817482ce06cad45e8a99e7aaf177c4f530bc8961
Shawn <singlex@foxmail.com>
2020-05-14 03:23:05 +0800

    add encrypt pdw, validate 1 - 65535
    Signed-off-by: Shawn <singlex@foxmail.com>

--- PRM.Core/Protocol/ProtocolServerWithAddrPortUserPwdBase.cs
+++ PRM.Core/Protocol/ProtocolServerWithAddrPortUserPwdBase.cs
@@ -28,16 +28,37 @@ namespace PRM.Core.Protocol
             get => _address;
             set
             {
-                SetAndNotifyIfChanged(nameof(Address), ref _address, value);
+                var v = value?.Trim();
+                if (string.IsNullOrEmpty(v))
+                    throw new ArgumentException("can't be empty!");
+                SetAndNotifyIfChanged(nameof(Address), ref _address, v);
             }
         }
 
 
-        private int _port = 3389;
-        public int Port
+        public int GetPort()
         {
-            get => _port > 0 ? _port : 3389;
-            set => SetAndNotifyIfChanged(nameof(Port), ref _port, value);
+            if (int.TryParse(Port, out var p))
+                return p;
+            return 0;
+        }
+        private string _port = "3389";
+        public string Port
+        {
+            get => !string.IsNullOrEmpty(_port) ? _port : "3389";
+            set
+            {
+                if (int.TryParse(value, out var p))
+                {
+                    if (p > 0 && p < 65536)
+                    {
+                        SetAndNotifyIfChanged(nameof(Port), ref _port, value);
+                        return;
+                    }
+                }
+                SetAndNotifyIfChanged(nameof(Port), ref _port, "0");
+                throw new ArgumentException("Port value error");
+            }
         }
 
 
