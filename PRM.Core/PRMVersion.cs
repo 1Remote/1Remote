886a5ea4788d8b57d919d5678d828f6a71bd1811
Shawn <singlex@foxmail.com>
2020-07-05 13:29:15 +0800

    create ButtonEx to handle Button hover click style.
    Signed-off-by: Shawn <singlex@foxmail.com>

--- PRM.Core/PRMVersion.cs
+++ PRM.Core/PRMVersion.cs
@@ -11,7 +11,7 @@ namespace PRM.Core
         public const int Major = 0;
         public const int Minor = 4;
         public const int Build = 4;
-        public const int ReleaseDate = 2007031630;
+        public const int ReleaseDate = 2007051326;
         public static string Version => $"{Major}.{Minor}.{Build}.{ReleaseDate}";
     }
 }
