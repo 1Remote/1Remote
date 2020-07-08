58d864f3f3802d5e35fbe25d2f293513b32cb2d2
Shawn <singlex@foxmail.com>
2020-07-08 19:15:22 +0800

    :hammer:[WIP] still working on color theme, using the new colorpicker in config page.
    Signed-off-by: Shawn <singlex@foxmail.com>

---
+++ PRM.Core/Resources/Theme/PrmThemes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRM.Core.Resources.Theme
+{
+    public class PrmTheme
+    {
+        public string Name { get; set; }
+        public string MainColor1 { get; set; }
+        public string MainColor1Lighter { get; set; }
+        public string MainColor1Darker { get; set; }
+        public string MainColor1Foreground { get; set; }
+        public string MainColor2 { get; set; }
+        public string MainColor2Lighter { get; set; }
+        public string MainColor2Darker { get; set; }
+        public string MainColor2Foreground { get; set; }
+        public string MainBgColor { get; set; }
+        public string MainBgColorForeground { get; set; }
+    }
+
+    public static class PrmThemes
+    {
+    }
+}
