3bf613c3ea5d9881c0aa929dc6bc5d0a3a58130d
Shawn <singlex@foxmail.com>
2020-07-09 23:49:23 +0800

    1. del border of MainWindow 2. add themes
    Signed-off-by: Shawn <singlex@foxmail.com>

--- PRM.Core/Resources/Theme/PrmColorThemes.cs
+++ PRM.Core/Resources/Theme/PrmColorThemes.cs
@@ -37,6 +37,41 @@ namespace PRM.Core.Resources.Theme
                 if (!themes.ContainsKey(theme.Key))
                     themes.Add(theme.Key, theme.Value);
             }
+
+            {
+                var theme = GetSecretKey();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
+
+            {
+                var theme = GetGreystone();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
+
+            {
+                var theme = GetAsphalt();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
+
+            {
+                var theme = GetWine();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
+
+            {
+                var theme = GetForest();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
+            {
+                var theme = GetSoil();
+                if (!themes.ContainsKey(theme.Key))
+                    themes.Add(theme.Key, theme.Value);
+            }
         }
 
         public static KeyValuePair<string, PrmColorTheme> GetDefault()
@@ -46,13 +81,113 @@ namespace PRM.Core.Resources.Theme
                 MainColor1 = "#102b3e",
                 MainColor1Lighter = "#445a68",
                 MainColor1Darker = "#0c2230",
-                MainColor1Foreground = "#ffffff",
+                MainColor1Foreground = "#FFFFFFFF",
                 MainColor2 = "#e83d61",
                 MainColor2Lighter = "#ed6884",
                 MainColor2Darker = "#b5304c",
-                MainColor2Foreground = "#ffffff",
+                MainColor2Foreground = "#FFFFFFFF",
                 MainBgColor = "#ced8e1",
                 MainBgColorForeground = "#000000",
+            });
+        }
+
+        public static KeyValuePair<string, PrmColorTheme> GetSecretKey()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("SecretKey", new PrmColorTheme()
+            {
+                MainColor1 = "#FF473368",
+                MainColor1Lighter = "#796090",
+                MainColor1Darker = "#382853",
+                MainColor1Foreground = "#FFFFFFFF",
+                MainColor2 = "#FFEF6D3B",
+                MainColor2Lighter = "#FF9A63",
+                MainColor2Darker = "#BF572F",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFF2F1EC",
+                MainBgColorForeground = "#000000",
+            });
+        }
+
+        public static KeyValuePair<string, PrmColorTheme> GetGreystone()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("Greystone", new PrmColorTheme()
+            {
+                MainColor1 = "#FFC7D0D5",
+                MainColor1Lighter = "#F9FDFD",
+                MainColor1Darker = "#9FA6AA",
+                MainColor1Foreground = "#FF1B2C3F",
+                MainColor2 = "#FFFF7247",
+                MainColor2Lighter = "#FFED583A",
+                MainColor2Darker = "#CC5B38",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFF5F5F5",
+                MainBgColorForeground = "#000000",
+            });
+        }
+
+        public static KeyValuePair<string, PrmColorTheme> GetAsphalt()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("Asphalt", new PrmColorTheme()
+            {
+                MainColor1 = "#FF393939",
+                MainColor1Lighter = "#6B6661",
+                MainColor1Darker = "#2D2D2D",
+                MainColor1Foreground = "#FFFFFFFF",
+                MainColor2 = "#FFFF7247",
+                MainColor2Lighter = "#FFED583A",
+                MainColor2Darker = "#CC5B38",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFF5F5F5",
+                MainBgColorForeground = "#000000",
+            });
+        }
+
+        public static KeyValuePair<string, PrmColorTheme> GetWine()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("Wine", new PrmColorTheme()
+            {
+                MainColor1 = "#FF57112D",
+                MainColor1Lighter = "#893E55",
+                MainColor1Darker = "#450D24",
+                MainColor1Foreground = "#FFFFFFFF",
+                MainColor2 = "#FFA82159",
+                MainColor2Lighter = "#DA4E81",
+                MainColor2Darker = "#861A47",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFFDEAD9",
+                MainBgColorForeground = "#FF450D24",
+            });
+        }
+        public static KeyValuePair<string, PrmColorTheme> GetForest()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("Forest", new PrmColorTheme()
+            {
+                MainColor1 = "#FF253938",
+                MainColor1Lighter = "#576660",
+                MainColor1Darker = "#1D2D2C",
+                MainColor1Foreground = "#FFFFFFFF",
+                MainColor2 = "#FF5FA291",
+                MainColor2Lighter = "#91CFB9",
+                MainColor2Darker = "#4C8174",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFF5F5F5",
+                MainBgColorForeground = "#FF303030",
+            });
+        }
+        public static KeyValuePair<string, PrmColorTheme> GetSoil()
+        {
+            return new KeyValuePair<string, PrmColorTheme>("Soil", new PrmColorTheme()
+            {
+                MainColor1 = "#FF776245",
+                MainColor1Lighter = "#A98F6D",
+                MainColor1Darker = "#FF735E41",
+                MainColor1Foreground = "#FFFFFFFF",
+                MainColor2 = "#FF0193B8",
+                MainColor2Lighter = "#33C0E0",
+                MainColor2Darker = "#007593",
+                MainColor2Foreground = "#FFFFFFFF",
+                MainBgColor = "#FFCFC3B5",
+                MainBgColorForeground = "#FF080000",
             });
         }
     }
