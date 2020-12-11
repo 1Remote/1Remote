e56958860ea68fa8e997fad2c53fa36fa84c96bf
Shawn <singlex@foxmail.com>
2020-12-11 22:32:22 +0800

    bug fix for Startuppath changed.
    Signed-off-by: Shawn <singlex@foxmail.com>

--- PRM.Core/PRMVersion.cs
+++ PRM.Core/PRMVersion.cs
@@ -11,7 +11,7 @@ namespace PRM.Core
         public const int Major = 0;
         public const int Minor = 5;
         public const int Build = 5;
-        public const int ReleaseDate = 2012092312;
+        public const int ReleaseDate = 2012112228;
         public static string Version => $"{Major}.{Minor}.{Build}.{ReleaseDate}";
     }
 }
