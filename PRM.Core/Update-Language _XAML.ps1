e59d1a26d9847b71fc77dac9b2f935b706d89c77
Daniel Berg <dberg@protakt.de>
2021-02-18 01:17:28 +0100

    Added german Language as json and xaml. Enhance Systemconfig.language.cs to be able to use german. Fixed two small text bugs in english version.

---
+++ PRM.Core/Update-Language _XAML.ps1
@@ -0,0 +1,28 @@
+﻿$JSON = Get-Content "C:\temp\de.json"  -encoding UTF8 | ConvertFrom-Json 
+[xml] $XML = Get-Content "C:\temp\de.xaml"  -encoding UTF8 
+
+for ($i =0;$i -lt $XML.ResourceDictionary.String.Count; $i++)
+{
+    $JSONMEMBER = Get-Member -InputObject $JSON -Name $XML.ResourceDictionary.String[$i].Key
+    if ($JSONMEMBER -ne $null)
+    {
+        $NewValue = $JSONMEMBER.Definition.Split("=")[1]
+        if ($NewValue -ne $null)
+        {
+            $XML.ResourceDictionary.String[$i].'#text' = $NewValue
+        }
+        else
+        {
+            Write-Warning ("Value is empty " + $i)
+            Write-Warning -Message $XML.ResourceDictionary.String[$i].Key
+            pause
+        }
+    }
+    else
+    {
+        Write-Warning ("JSONMEMBER is empty " + $i)
+        Write-Warning -Message $XML.ResourceDictionary.String[$i].Key
+        pause
+    }
+}
+$XML.Save("C:\temp\de2.xaml")
