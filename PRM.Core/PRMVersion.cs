abba69ca20070598a0ff6557bef6fc973c16c732
Shawn <singlex@foxmail.com>
2021-03-11 18:19:44 +0800

    fix bug: Crashed when user who are using multi-screen in different scale factor and open a all screen & disabled RDGateway RDP session, close #94.

--- PRM.Core/PRMVersion.cs
+++ PRM.Core/PRMVersion.cs
@@ -5,7 +5,7 @@ namespace PRM.Core
         public const int Major = 0;
         public const int Minor = 5;
         public const int Patch = 10;
-        public const string PreRelease = "alpha"; // e.g. "alpha" "beta.2"
+        public const string PreRelease = "alpha2"; // e.g. "alpha" "beta.2"
         public static string Version => string.IsNullOrWhiteSpace(PreRelease) ? $"{Major}.{Minor}.{Patch}" : $"{Major}.{Minor}.{Patch}-{PreRelease}";
     }
 }
