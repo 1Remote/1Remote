d94472beababd3058e4e6c2e42141cb0dca89bf6
Shawn <singlex@foxmail.com>
2021-04-25 11:03:52 +0800

    additional usb support for RDP full scree #113

--- PRM.Core/Resources/Converter/Converter.cs
+++ PRM.Core/Resources/Converter/Converter.cs
@@ -86,6 +86,26 @@ namespace PRM.Core.Resources.Converter
         #endregion IValueConverter 成员
     }
 
+    public class ConverterBoolInverse : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+
     //public class ConverterDouble2Negate : IValueConverter
     //{
     //    #region IValueConverter 成员
--- PRemoteM/View/FullScreenWindow.xaml.cs
+++ PRemoteM/View/FullScreenWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using PRM.Core.Protocol;
+﻿using System;
+using PRM.Core.Protocol;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Interop;
+using PRM.Core.Protocol.RDP.Host;
 using PRM.Model;
+using Shawn.Utils;
 
 namespace PRM.View
 {
@@ -14,6 +18,8 @@ namespace PRM.View
             Loaded += (sender, args) =>
             {
                 this.Content = ProtocolHostBase;
+                var source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+                source.AddHook(new HwndSourceHook(WndProc));
             };
             Closed += (sender, args) =>
             {
@@ -22,6 +28,21 @@ namespace PRM.View
                     RemoteWindowPool.Instance.DelProtocolHostInSyncContext(ProtocolHostBase.ConnectionId);
                 }
             };
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_DEVICECHANGE = 0x0219;
+            if (msg == WM_DEVICECHANGE)
+            {
+                if (ProtocolHostBase is AxMsRdpClient09Host rdp)
+                {
+                    SimpleLogHelper.Debug($"rdp.NotifyRedirectDeviceChange((uint){wParam}, (int){lParam})");
+                    rdp.NotifyRedirectDeviceChange(msg, (uint) wParam, (int) lParam);
+                }
+            }
+
+            return IntPtr.Zero;
         }
 
         public void SetProtocolHost(ProtocolHostBase content)
