4423dd7ce66ea6739cb3942b121cfdc3ff08051e
Shawn <singlex@foxmail.com>
2021-05-12 08:54:25 +0800

    disable double click the title bar of tab window to let it go to maximized when it is locked

--- PRemoteM/View/TabWindow/TabWindowChrome.xaml.cs
+++ PRemoteM/View/TabWindow/TabWindowChrome.xaml.cs
@@ -9,15 +9,5 @@ namespace PRM.View.TabWindow
             InitializeComponent();
             base.Init(TabablzControl);
         }
-
-        protected override void WinTitleBar_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                if (!Vm.SelectedItem.CanResizeNow)
-                    return;
-            }
-            base.WinTitleBar_MouseDown(sender, e);
-        }
     }
 }
