2cd031b9d89cb2dff1101942e96adb0108d152ed
Shawn <singlex@foxmail.com>
2021-05-21 10:24:51 +0800

    fix #121

--- PRM.Core/Properties/AssemblyInfo.cs
+++ PRM.Core/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PRM.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Shawn")]
+[assembly: AssemblyConfiguration("Shawn")]
+[assembly: AssemblyCompany("Shawn")]
 [assembly: AssemblyProduct("PRM.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("bb2390fe-00dc-4650-a856-ddb69fd92bbc")]
+[assembly: Guid("bb2390f1-00dc-4650-a856-ddb69fd92bbc")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
