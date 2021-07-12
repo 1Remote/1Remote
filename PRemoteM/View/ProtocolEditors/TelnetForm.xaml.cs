63f11438960a4ae76147bbf1cc5530cf54792daf
Shawn <singlex@foxmail.com>
2021-07-12 22:11:54 +0800

    move view from Core to App

---
+++ PRemoteM/View/ProtocolEditors/TelnetForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PRM.Core.Protocol;
+
+namespace PRM.View.ProtocolEditors
+{
+    public partial class TelnetForm : ProtocolServerFormBase
+    {
+        public readonly ProtocolServerBase Vm;
+        public TelnetForm(ProtocolServerBase vm) : base(vm)
+        {
+            InitializeComponent();
+            Vm = vm;
+            DataContext = vm;
+        }
+    }
+}
