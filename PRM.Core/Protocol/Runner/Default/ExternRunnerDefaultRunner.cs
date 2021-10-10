db1ed6a8e93add73a92c740f812d61466905212d
Shawn <singlex@foxmail.com>
2021-10-10 14:05:28 +0800

    external runner is available now. WinSTP test passed!

---
+++ PRM.Core/Protocol/Runner/Default/ExternRunnerDefaultRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using JsonKnownTypes;
+using Newtonsoft.Json;
+
+namespace PRM.Core.Protocol.Runner.Default
+{
+    public class ExternalDefaultRunner : ExternalRunner
+    {
+        public ExternalDefaultRunner() : base("External runner")
+        {
+        }
+    }
+}
