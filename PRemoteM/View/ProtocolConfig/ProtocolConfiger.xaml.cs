63783f930c79df56404f6af2930f7198754b537f
Shawn <singlex@foxmail.com>
2021-11-28 10:05:33 +0800

    fix ExternalRunnerConfiger viewmode binding error.

--- PRemoteM/View/ProtocolConfig/ProtocolConfiger.xaml.cs
+++ PRemoteM/View/ProtocolConfig/ProtocolConfiger.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PRM.Core.Model;
+using PRM.Core.Protocol.Runner;
+using PRM.Core.Service;
 using PRM.ViewModel.Configuration;
 
 namespace PRM.View.ProtocolConfig
@@ -28,5 +30,31 @@ namespace PRM.View.ProtocolConfig
             var vm = new ProtocolConfigerViewModel(App.Context.ProtocolConfigurationService, App.Context.LanguageService);
             this.DataContext = vm;
         }
+    }
+
+
+    public class ExternalRunner2Vm : IValueConverter
+    {
+        #region IValueConverter 成员
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is ExternalRunner er)
+            {
+                return new ExternalRunnerConfigerVM(er, LanguageService.TmpLanguageService);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is ExternalRunnerConfigerVM vm)
+            {
+                return vm.ExternalRunner;
+            }
+            return null;
+        }
+
+        #endregion IValueConverter 成员
     }
 }
