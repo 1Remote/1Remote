8f6db088d58733f3af3509fd7947f9651a78ffbc
Shawn <singlex@foxmail.com>
2022-03-14 20:00:01 +0800

    Port can be save then add RDP with an encrypted db. fix #217.

--- PRM.Core/PRMVersion.cs
+++ PRM.Core/PRMVersion.cs
@@ -8,7 +8,7 @@ namespace PRM.Core
         public const uint Major = 0;
         public const uint Minor = 6;
         public const uint Patch = 2;
-        public const uint Build = 0;
+        public const uint Build = 1;
         public const string PreRelease = ""; // e.g. "alpha" "beta.2"
 
         public static readonly VersionHelper.Version VersionData = new VersionHelper.Version(Major, Minor, Patch, Build, PreRelease);
@@ -18,6 +18,7 @@ namespace PRM.Core
         public static readonly string[] UpdateUrls =
         {
             "https://github.com/VShawn/PRemoteM",
+            "https://gitee.com/vshawn/PRemoteM/blob/dev/readme.md",
 #if DEV
             "https://github.com/VShawn/PRemoteM-Test/wiki",
 #endif
--- PRM.Core/Protocol/RDP/ProtocolServerRDP.cs
+++ PRM.Core/Protocol/RDP/ProtocolServerRDP.cs
@@ -434,7 +434,7 @@ namespace PRM.Core.Protocol.RDP
         /// <returns></returns>
         public RdpConfig ToRdpConfig(PrmContext context)
         {
-            var rdpConfig = new RdpConfig($"{this.Address}:{this.Port}", this.UserName, context?.DataService?.DecryptOrReturnOriginalString(Password), RdpFileAdditionalSettings)
+            var rdpConfig = new RdpConfig($"{this.Address}:{this.GetPort()}", this.UserName, context?.DataService?.DecryptOrReturnOriginalString(Password), RdpFileAdditionalSettings)
             {
                 Domain = this.Domain,
                 LoadBalanceInfo = this.LoadBalanceInfo,
