4f9617d7eda8b1850e0dbcd1c7c7498bacc60cb9
Shawn <singlex@foxmail.com>
2022-04-02 17:37:02 +0800

    [WIP] IOC remould: Adjust the creation sequence

--- PRemoteM/App.xaml.cs
+++ PRemoteM/App.xaml.cs
@@ -21,78 +21,21 @@ using Shawn.Utils.Wpf.FileSystem;
 
 namespace PRM
 {
-    /*
-    Defines:
-        FOR_MICROSOFT_STORE_ONLY        =>  Disable all functions store not recommend.Must define FOR_MICROSOFT_STORE first!!!
-    */
-
     public partial class App : Application
     {
         public static ResourceDictionary ResourceDictionary { get; private set; }
-        public static bool IsNewUser = false;
-
-        //public static Dispatcher UiDispatcher = null;
-
-        private static void OnUnhandledException(Exception e)
-        {
-            App.Current.Dispatcher.Invoke(() =>
-            {
-                lock (App.Current)
-                {
-                    SimpleLogHelper.Fatal(e);
-                    var errorReport = new ErrorReportWindow(e);
-                    errorReport.ShowDialog();
-#if FOR_MICROSOFT_STORE_ONLY
-                    throw e;
-#else
-                    App.Close();
-#endif 
-                }
-            });
-        }
-
-        private void InitExceptionHandle()
-        {
-            this.DispatcherUnhandledException += (o, args) =>
-            {
-                OnUnhandledException(args.Exception);
-            };
-            TaskScheduler.UnobservedTaskException += (o, args) =>
-            {
-                OnUnhandledException(args.Exception);
-            };
-            AppDomain.CurrentDomain.UnhandledException += (o, args) =>
-            {
-                if (args.ExceptionObject is Exception e)
-                {
-                    OnUnhandledException(e);
-                }
-                else
-                {
-                    SimpleLogHelper.Fatal(args.ExceptionObject);
-                }
-            };
-        }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory); // in case user start app in a different working dictionary.
             ResourceDictionary = this.Resources;
-
-
-
-            // BASE MODULES
-            InitExceptionHandle();
-            
             base.OnStartup(e);
         }
 
-
-
         public static void Close(int exitCode = 0)
         {
-            IoC.Get<MainWindowView>()?.CloseMe();
             IoC.Get<LauncherWindowView>()?.Close();
+            IoC.Get<MainWindowView>()?.CloseMe();
             Environment.Exit(exitCode);
         }
     }
