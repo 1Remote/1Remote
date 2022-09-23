d1a5fed4bbe39311a03e2e415995cba4bbdd1320
Albert Frankfurt <100777051+tptyphon@users.noreply.github.com>
2022-09-24 00:40:00 +0300

    add ukrainian language translation to app

--- Ui/Resources/Languages/glossary_maker.py
+++ Ui/Resources/Languages/glossary_maker.py
@@ -139,9 +139,7 @@ if __name__ == '__main__':
     g.load_csv(CSV_FILE_NAME + '.csv')
     # g.save_csv(CSV_FILE_NAME + '.csv')
 
-    http_proxy = SyncHTTPProxy((b'http', b'127.0.0.1', 1080, b''))
-    proxies = {'http': http_proxy, 'https': http_proxy}
-    translator = Translator(proxies=proxies)
+    translator = Translator()
     # print(translator.translate('hi\r\nsun rise', dest='de').text)
 
     # translate
