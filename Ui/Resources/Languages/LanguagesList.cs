1cdfbe5a9e9ea7d8940e6fdac6c445d42c272526
VShawn <veckshawn@gmail.com>
2022-09-30 08:46:52 +0800

    Revert "add ukrainian language translation to app"

--- Ui/Resources/Languages/LanguagesList.cs
+++ Ui/Resources/Languages/LanguagesList.cs
@@ -3,14 +3,12 @@ public static class LanguagesResources
 {
     public static readonly string[] Files = new string[]
     {
-        "language_code-iso.xaml",
         "en-us.xaml",
         "zh-cn.xaml",
         "cs-cz.xaml",
         "de-de.xaml",
         "fr-fr.xaml",
         "pt-br.xaml",
-        "ja-jp.xaml",
-        "uk-ua.xaml"
+        "ja-jp.xaml"
     };
 }
