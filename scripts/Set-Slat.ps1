8350a5a3070c4d8fed968a6fce2885ba2e378519
Shawn <veckshawn@gmail.com>
2023-02-02 18:43:58 +0800

    refacing tab CanResizeNow control, maybe helpful for #346

---
+++ scripts/Set-Slat.ps1
@@ -0,0 +1,31 @@
+param (
+    [string]$Slat
+)
+
+# Check if Slat is empty
+if (!$Slat) {
+    throw "Error: Slat is empty."
+}
+
+# Get the current working directory
+$originalDirectory = Get-Location
+
+# Set the current directory to the script directory
+Set-Location $PSScriptRoot
+
+# Get the file
+$file = Get-ChildItem -Path ..\Ui -Filter AppInit.cs
+
+# Check if the file exists
+if (!$file) {
+    throw "Error: AppInit.cs does not exist."
+}
+
+# Replace the content of the file (***SALT***) with the input Slat
+$file | ForEach-Object {
+# 
+    (Get-Content $_.FullName) | ForEach-Object { $_ -replace "\*\*\*SALT\*\*\*", $Slat } | Set-Content $_.FullName
+}
+
+# Set the current directory back to the original location
+Set-Location $originalDirectory
