cd457a232ef3b8a59ee925a12e5e7462fc982db3
Shawn <veckshawn@gmail.com>
2023-02-20 20:34:22 +0800

    #301 Add a tip `Detecting your alternate host...`

---
+++ Ui/View/Utils/WaitingViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using _1RM.Utils;
+using Shawn.Utils.Wpf;
+using _1RM.Service;
+
+namespace _1RM.View.Utils
+{
+    /// <summary>
+    /// Default implementation of IMessageBoxViewModel, and is therefore the ViewModel shown by default by ShowMessageBox
+    /// </summary>
+    public class WaitingViewModel : NotifyPropertyChangedBaseScreen
+    {
+        private string _title = "";
+        public string Title
+        {
+            get => _title;
+            set => SetAndNotifyIfChanged(ref _title, value);
+        }
+        private string _message = "";
+        public string Message
+        {
+            get => _message;
+            set => SetAndNotifyIfChanged(ref _message, value);
+        }
+    }
+}
