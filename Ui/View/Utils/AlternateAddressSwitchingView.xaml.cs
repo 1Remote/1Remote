c403748146bff3835f545661df3438cb558658d3
Shawn <veckshawn@gmail.com>
2023-05-31 21:59:11 +0800

    [WIP] #420 redesign availability detection screen

---
+++ Ui/View/Utils/AlternateAddressSwitchingView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using _1RM.Utils;
+using Shawn.Utils.Wpf;
+using _1RM.Service;
+using Shawn.Utils.WpfResources.Theme.Styles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Media3D;
+
+namespace _1RM.View.Utils
+{
+    public partial class AlternateAddressSwitchingView : WindowChromeBase
+    {
+        public double TopFrom
+        {
+            get; set;
+        }
+        public static HashSet<AlternateAddressSwitchingView> _dialogs = new HashSet<AlternateAddressSwitchingView>();
+        public AlternateAddressSwitchingView()
+        {
+            Loaded += OnLoaded;
+            Closed += OnClosed;
+        }
+
+        private void OnClosed(object? sender, EventArgs e)
+        {
+            _dialogs.Remove(this);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TopFrom = GetTopFrom();
+            Top = TopFrom - ActualHeight;
+            Left = SystemParameters.WorkArea.Right - ActualWidth;
+            _dialogs.Add(this);
+        }
+
+
+        private double GetTopFrom()
+        {
+            //屏幕的高度-底部TaskBar的高度。
+            double topFrom = System.Windows.SystemParameters.WorkArea.Bottom;
+            bool isContinueFind = _dialogs.Any(o => Math.Abs(o.TopFrom - topFrom) < 0.001);
+
+            while (isContinueFind)
+            {
+                topFrom -= this.ActualHeight;
+                isContinueFind = _dialogs.Any(o => Math.Abs(o.TopFrom - topFrom) < 0.001);
+            }
+
+            if (topFrom <= 0)
+                topFrom = System.Windows.SystemParameters.WorkArea.Bottom - 10;
+
+            return topFrom;
+        }
+
+        private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is AlternateAddressSwitchingViewModel vm)
+            {
+                vm.CmdCloseContinue.Execute(null);
+            }
+
+            //this.Dispatcher.Invoke(delegate
+            //{
+            //    double right = SystemParameters.WorkArea.Right;
+            //    DoubleAnimation animation = new DoubleAnimation();
+            //    animation.Duration = new Duration(TimeSpan.FromMilliseconds(500));
+            //    animation.Completed += (s, a) => { this.Close(); };
+            //    animation.From = right - this.ActualWidth;
+            //    animation.To = right;
+            //    animation.Completed += (o, args) =>
+            //    {
+            //        if (this.DataContext is AlternateAddressSwitchingViewModel vm)
+            //        {
+            //            vm.CmdCloseContinue.Execute(null);
+            //        }
+            //    };
+            //    this.BeginAnimation(Window.LeftProperty, animation);
+            //});
+        }
+    }
+}
---
+++ Ui/View/Utils/AlternateAddressSwitchingViewModel.cs
@@ -0,0 +1,101 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using _1RM.Utils;
+using Shawn.Utils.Wpf;
+using _1RM.Service;
+using Shawn.Utils;
+
+namespace _1RM.View.Utils
+{
+    public class PingTestItem : NotifyPropertyChangedBase
+    {
+        public enum PingStatus
+        {
+            None,
+            Testing,
+            Failed,
+            Success
+        }
+
+
+        public PingTestItem(string name)
+        {
+            Name = name;
+        }
+
+        public string Name {get; }
+
+        private PingStatus _status = PingStatus.None;
+        public PingStatus Status
+        {
+            get => _status;
+            set => SetAndNotifyIfChanged(ref _status, value);
+        }
+
+        private int _ping = 0;
+        public int Ping
+        {
+            get => _ping;
+            set => SetAndNotifyIfChanged(ref _ping, value);
+        }
+    }
+
+    public class AlternateAddressSwitchingViewModel : NotifyPropertyChangedBaseScreen
+    {
+        private string _title = "";
+        public string Title
+        {
+            get => _title;
+            set => SetAndNotifyIfChanged(ref _title, value);
+        }
+
+        private int _eta = 0;
+        public int Eta
+        {
+            get => _eta;
+            set => SetAndNotifyIfChanged(ref _eta, value);
+        }
+
+        private List<PingTestItem> _pingTestItems = new List<PingTestItem>();
+        public List<PingTestItem> PingTestItems
+        {
+            get => _pingTestItems;
+            set => SetAndNotifyIfChanged(ref _pingTestItems, value);
+        } 
+
+
+        private RelayCommand? _cmdCloseContinue;
+        public RelayCommand CmdCloseContinue
+        {
+            get
+            {
+                return _cmdCloseContinue ??= new RelayCommand((o) =>
+                {
+                    this.RequestClose();
+                });
+            }
+        }
+
+        private RelayCommand? _cmdCloseEnd;
+
+        public RelayCommand CmdCloseEnd
+        {
+            get
+            {
+                return _cmdCloseEnd ??= new RelayCommand((o) =>
+                {
+                    // TODO confirm
+                    this.RequestClose();
+                });
+            }
+        }
+    }
+}
