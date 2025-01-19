e628ab780eb0da790ac1c3ae8a3d3ec2cc306be8
Shawn <veckshawn@gmail.com>
2025-01-19 21:15:29 +0800

    fix: Fix the incorrect behavior of the right-click menu in the tab header. fix #645

--- Ui/Utils/RdpFile/MsRdpClientNonScriptableWrapper.cs
+++ Ui/Utils/RdpFile/MsRdpClientNonScriptableWrapper.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Interop;
 
 namespace _1RM.Utils.RdpFile
 {
