fbef8f59a1e134c02a3fbf20b948f10921c8b8cd
Shawn <veckshawn@gmail.com>
2025-03-22 09:13:59 +0800

    chore: fit for net48 store version

--- Ui/AppVersion.cs
+++ Ui/AppVersion.cs
@@ -5,8 +5,8 @@ namespace _1RM
     public static class AppVersion
     {
         public const uint Major = 1;
-        public const uint Minor = 2;
-        public const uint Patch = 0;
+        public const uint Minor = 1;
+        public const uint Patch = 2;
         public const uint Build = 0;
         public const string BuildDate = "";
         public const string PreRelease = ""; // e.g. "alpha" "beta.2"
